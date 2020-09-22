cbe1536ec099634b3b880b35af81bd68bfacb77d
Kathryn :p <71695603+KathrynTaggartOPHS@users.noreply.github.com>
2020-09-22 10:10:59 +0100

    Add files via upload

---
+++ 07- String slicing challenges 02- 14.09.2020- 10QCS1.py
@@ -0,0 +1,9 @@
+firstName = str(input("Enter your forename >>>"))
+lastName = str(input("Enter your surname >>>"))
+space = (" ")
+name = firstName+space+lastName
+nameLength = len(name)
+
+print(name)
+print(nameLength)
+
