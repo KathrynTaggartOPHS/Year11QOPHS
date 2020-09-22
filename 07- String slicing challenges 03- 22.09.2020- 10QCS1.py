039da4643d2ad6f20acb4b399d48819898e8260b
Kathryn :p <71695603+KathrynTaggartOPHS@users.noreply.github.com>
2020-09-22 10:13:43 +0100

    Add files via upload

---
+++ 07- String slicing challenges 03- 22.09.2020- 10QCS1.py
@@ -0,0 +1,6 @@
+firstName = str(input("Enter your first name (lowercase) >>>>"))
+lastName = str(input("Enter your last name (lowercase) >>>>"))
+space = (" ")
+
+wholeName = firstName + space + lastName
+print(wholeName)
