34b563e44d4487b0293635edb136d44b3409f184
Kathryn :p <71695603+KathrynTaggartOPHS@users.noreply.github.com>
2021-02-11 11:13:29 +0000

    Delete StringsSlicingChallenges03.py

--- StringsSlicingChallenges03.py
+++
@@ -1,6 +0,0 @@
-firstName = str(input("Enter your first name (lowercase) >>>>"))
-lastName = str(input("Enter your last name (lowercase) >>>>"))
-space = (" ")
-
-wholeName = firstName + space + lastName
-print(wholeName)
