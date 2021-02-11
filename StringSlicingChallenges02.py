e1bf379f53c08209ec7d9ea8126772fb9ef18c2b
Kathryn :p <71695603+KathrynTaggartOPHS@users.noreply.github.com>
2021-02-11 11:13:17 +0000

    Delete StringSlicingChallenges02.py

--- StringSlicingChallenges02.py
+++
@@ -1,9 +0,0 @@
-firstName = str(input("Enter your forename >>>"))
-lastName = str(input("Enter your surname >>>"))
-space = (" ")
-name = firstName+space+lastName
-nameLength = len(name)
-
-print(name)
-print(nameLength)
-
